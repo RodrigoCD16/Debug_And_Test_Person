af95aa4134cce11b34fef46f0ebe14096386d58f
RodrigoCD16 <53954034+RodrigoCD16@users.noreply.github.com>
2021-09-08 11:03:01 -0300

    Update PersonTests.cs

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,6 +6,9 @@ namespace Tests
 {
     public class Tests
     {
+        Person p1 = new Person("John Doe", "1.234.567-8");
+        IdUtils verifier = new IdUtils();
+
         [SetUp]
         public void Setup()
         {
@@ -13,9 +16,27 @@ namespace Tests
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void EmptyNameTest() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            p1.Name = "";
+            const string expected = "John Doe";
+            Assert.AreEqual(expected, p1.Name);
+        }
+        
+        [Test]
+        public void ValidNameChangeTest() // Cambiá el nombre para indicar qué estás probando
+        {
+            p1.Name = "John Snow";
+            const string expected = "John Snow";
+            Assert.AreEqual(expected, p1.Name);
+        }
+
+        [Test]
+        public void InvalidLongerIDTest() // Cambiá el nombre para indicar qué estás probando
+        {
+            verifier.IdIsValid("1.234.567.910-8");
+            const string expected = "False";
+            Assert.AreEqual(expected, p1.ID);
         }
     }
 }
